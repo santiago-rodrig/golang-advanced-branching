90ff7216fb4732dfd3ae99f4aa3e637f2d6238f2
santiago-rodrig <santo1996.29@gmail.com>
2021-03-02 12:25:35 -0400

    define initial types and variables

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,46 @@
 package main
 
-import (
+type vehicle interface{}
 
-)
+type car struct {
+	model       string
+	make        string
+	typeVehicle string
+}
 
+type truck struct {
+	model       string
+	make        string
+	typeVehicle string
+}
+
+type bike struct {
+	model string
+	make  string
+}
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
+}
+
+type feedbackResult struct {
+	feedbackTotal    int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral  int
+}
+
+var (
+	vehicleResult map[string]feedbackResult
+	inventory     []vehicle
+)
 
 type rating float32
 
@@ -20,8 +53,6 @@ const (
 )
 
 func init() {
-
-/*
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -33,16 +64,16 @@ func init() {
 		car{"CRV", "Honda", "SUV"},
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
-		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+		truck{"RAM1500", "Dodge", "Truck"},
+	}
 
+	vehicleResult = make(map[string]feedbackResult)
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
